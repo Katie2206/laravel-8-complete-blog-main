786ffb935c2e977ae80adba92360a0c3cb5a8699
Katie2206 <katielynch2204@gmail.com>
2024-03-11 10:52:17 +0000

    Revert "added php files"
    This reverts commit 10154a2f9d2fefcf0d8bb6a49ad328232564303b.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PostsController;
-use App\Http\Controllers\CompanyCRUDController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +16,7 @@ use App\Http\Controllers\CompanyCRUDController;
 */
 
 Route::get('/', [PagesController::class, 'index']);
-Route::resource('companies', CompanyCRUDController::class);
+
 Route::resource('/blog', PostsController::class);
 
 Auth::routes();
