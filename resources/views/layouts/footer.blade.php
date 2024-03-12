de0b614963b269bec505657105a310cfb26ac9ca
Katie2206 <katielynch2204@gmail.com>
2024-03-12 10:18:38 +0000

    Changing style
    Changing style of main homepage

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,11 +1,11 @@
-<footer class="bg-red-800 py-20 mt-20">
+<footer class="bg-gray-500 py-20 mt-20">
     <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-gray-900">
                 Pages
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-gray-800">
                 <li class="pb-1">
                     <a href="/">
                         Home
@@ -35,11 +35,11 @@
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-gray-900">
                 Find Us
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-gray-800">
                 <li class="pb-1">
                     <a href="/">
                         What we do
@@ -64,11 +64,11 @@
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
+            <h3 class="text-l sm:font-bold text-gray-900">
                 Latest posts
             </h3>
 
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+            <ul class="py-4 sm:text-s pt-4 text-gray-800">
                 <li class="pb-1">
                     <a href="/">
                         Why we love tech
