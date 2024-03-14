18875761a396b97435fcfbd0ac81f7d75273ead5
Katie2206 <katielynch2204@gmail.com>
2024-03-14 23:27:45 +0000

    Added logo
    Added logo to the header so it displays on all pages

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\AntagonistController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
