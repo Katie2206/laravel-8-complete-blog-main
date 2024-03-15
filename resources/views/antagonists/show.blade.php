36b140451ee9e581dbc18f6f2ebfc52f35398989
Katie2206 <katielynch2204@gmail.com>
2024-03-15 15:09:57 +0000

    Alterations to posts
    the $post used previously was $antagonist_post but this would not work for the update and had to be changed to the correct $post
    changed the layout of the index page and added a login/out and register choice in the footer

--- resources/views/antagonists/show.blade.php
+++ resources/views/antagonists/show.blade.php
@@ -5,21 +5,21 @@
 <div class="w-4/5 m-auto text-left font-mono text-gray-300">
     <div class="py-15">
         <h1 class="text-6xl">
-            {{ $antagonist_post->title }}
+            {{ $post->title }}
         </h1>
     </div>
 </div>
 <div class="pl-36">
-    <img src="{{ asset('images/' . $antagonist_post->image_path) }}" alt="">
+    <img src="{{ asset('images/' . $post->image_path) }}" alt="">
 </div>
 
 <div class="w-4/5 m-auto pt-20">
     <span class="text-gray-400">
-        By <span class="font-bold italic text-gray-600">{{ $antagonist_post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($antagonist_post->updated_at)) }}
+        By <span class="font-bold italic text-gray-600">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
     </span>
 
     <p class="text-xl text-gray-500 pt-8 pb-10 leading-8 font-mono font-light">
-        {{ $antagonist_post->description }}
+        {{ $post->description }}
     </p>
 </div>
 </div>
--- resources/views/protagonists/index.blade.php
+++ resources/views/protagonists/index.blade.php
@@ -1,84 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="bg-drop-9">
-<div class="w-4/5 m-auto text-center">
-    <div class="py-15 border-b border-gray-200">
-        <h1 class="text-6xl text-gray-300 font-mono uppercase">
-            Protagonists
-        </h1>
-    </div>
-</div>
-
-@if (session()->has('message'))
-    <div class="w-4/5 m-auto mt-10 pl-2">
-        <p class="w-1/6 mb-4 text-center text-gray-50 bg-green-500 rounded-2xl py-4">
-            {{ session()->get('message') }}
-        </p>
-    </div>
-@endif
-
-@if (Auth::check())
-    <div class="pt-15 w-4/5 m-auto border-b-30">
-        <a 
-            href="/protagonists/create"
-            class="bg-drop-6 uppercase bg-transparent text-gray-100 text-xs font-mono font-extrabold py-3 px-5 rounded-3xl">
-            Add Protagonists Blog
-        </a>
-    </div>
-@endif
-</div>
-
-@foreach ($posts as $post)
-<div class="bg-drop-9">
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-transparent">
-        <div>
-            <img src="{{ asset('images/' . $post->image_path) }}" alt="">
-        </div>
-        <div>
-            <h2 class="text-gray-500 font-bold font-mono text-4xl pb-4">
-                {{ $post->title }}
-            </h2>
-
-            <span class="text-gray-500">
-                By <span class="font-bold italic text-gray-600">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-            </span>
-
-            <p class="text-xl text-gray-400 pt-8 pb-10 leading-8 font-mono font-light">
-                {{ $post->description }}
-            </p>
-        <div class="flexbutton">
-            <div>
-            <a href="/protagonists/{{ $post->slug }}" class="uppercase bg-drop-1 text-gray-100 text-lg font-mono font-extrabold py-4 px-8 rounded-3xl">
-                Keep Reading
-            </a>
+    <div class="bg-drop-9">
+        <div class="w-4/5 m-auto text-center">
+            <div class="py-15 border-b border-gray-200">
+                <h1 class="text-6xl text-gray-300 font-mono uppercase">
+                    Protagonists
+                </h1>
             </div>
         </div>
-            @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
-                <span class="float-right">
-                    <a 
-                        href="/protagonists/{{ $post->slug }}/edit"
-                        class="text-gray-500 italic hover:text-gray-600 pb-1 border-b-2">
-                        Edit
-                    </a>
-                </span>
 
-                <span class="float-right">
-                     <form 
-                        action="/protagonists/{{ $post->slug }}"
-                        method="POST">
-                        @csrf
-                        @method('delete')
+        @if (session()->has('message'))
+            <div class="w-4/5 m-auto mt-10 pl-2">
+                <p class="w-1/6 mb-4 text-center text-gray-50 bg-green-500 rounded-2xl py-4">
+                    {{ session()->get('message') }}
+                </p>
+            </div>
+        @endif
 
-                        <button
-                            class="text-red-500 pr-3"
-                            type="submit">
-                            Delete
-                        </button>
+        @if (Auth::check())
+            <div class="pt-15 w-4/5 m-auto border-b-30">
+                <a href="/protagonists/create"
+                    class="bg-drop-6 uppercase bg-transparent text-gray-100 text-xs font-mono font-extrabold py-3 px-5 rounded-3xl">
+                    Add Protagonists Blog
+                </a>
+            </div>
+        @endif
+    </div>
 
-                    </form>
-                </span>
-                {{-- <div class="py-12">
+    @foreach ($posts as $post)
+        <div class="bg-drop-9">
+            <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-transparent">
+                <div>
+                    <img src="{{ asset('images/' . $post->image_path) }}" alt="">
+                </div>
+                <div>
+                    <h2 class="text-gray-500 font-bold font-mono text-4xl pb-4">
+                        {{ $post->title }}
+                    </h2>
+
+                    <span class="text-gray-500">
+                        By <span class="font-bold italic text-gray-600">{{ $post->user->name }}</span>, Created on
+                        {{ date('jS M Y', strtotime($post->updated_at)) }}
+                    </span>
+
+                    <p class="text-xl text-gray-400 pt-8 pb-10 leading-8 font-mono font-light">
+                        {{ $post->description }}
+                    </p>
+                    <div class="flexbutton">
+                        <div>
+                            <a href="/protagonists/{{ $post->slug }}"
+                                class="uppercase bg-drop-1 text-gray-100 text-lg font-mono font-extrabold py-4 px-8 rounded-3xl">
+                                Keep Reading
+                            </a>
+                        </div>
+                    </div>
+                    @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                        <span class="float-right">
+                            <a href="/protagonists/{{ $post->slug }}/edit"
+                                class="text-gray-500 italic hover:text-gray-600 pb-1 border-b-2">
+                                Edit
+                            </a>
+                        </span>
+
+                        <span class="float-right">
+                            <form action="/protagonists/{{ $post->slug }}" method="POST">
+                                @csrf
+                                @method('delete')
+
+                                <button class="text-red-500 pr-3" type="submit">
+                                    Delete
+                                </button>
+
+                            </form>
+                        </span>
+                        {{-- <div class="py-12">
                     <form id="like" method="POST">
                         <select name="Likeability">
                             <option value="1">1</option>
@@ -93,10 +89,9 @@
             //     echo "Likeability: ".htmlspecialchars($_POST['Likeability']);
             // } --}}
                 </div>
-            @endif
-        </div>
-    </div>  
-</div>  
-@endforeach
-
+    @endif
+    </div>
+    </div>
+    </div>
+    @endforeach
 @endsection
